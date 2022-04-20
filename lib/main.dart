1c2b7c87496eb3b90974eca5a78edad391ea5cfe
Dita Permata Putra <ditateum@gmail.com>
2022-04-20 23:50:58 +0700

    product overview and product item done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './screens/products_overview_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -9,25 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Shop Ap"),
-      ),
-      body: const Center(
-        child: Text("Welcome to shop app"),
-      ),
+      home: ProductsOverviewScreen(),
     );
   }
 }
